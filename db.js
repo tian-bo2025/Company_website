afe5f9f696d7833bdf87e90afe319ab4615017d2
tian-bo2025 <bodawujiang2024@gmail.com>
2025-09-30 20:36:22 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
-// db.js
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
+// 使用 __dirname 拼接绝对路径
+// db.js 在 E:\公司网站\ 下，所以直接拼接 mysite.db
 const dbPath = path.join(__dirname, 'mysite.db');
+
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
-    console.error('无法连接到数据库', err.message);
+    console.error('无法连接数据库:', err.message);
   } else {
-    console.log('成功连接到 SQLite 数据库');
+    console.log('成功连接数据库:', dbPath);
   }
 });
 
